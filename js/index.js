c6077aec7e80047a11356fd3589bbc8eaf3271fb
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-08-26 18:53:59 +0100

    class added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,42 @@
+const countryInput = document.querySelectorAll(".country-input");
+const main = document.querySelector(".main");
+
+
+countryInput.forEach(item => {
+    item.onkeyup = (e) => {
+        let countryData = e.target.value;
+        if (countryData) {
+            main.classList.add("active");
+        } else {
+            main.classList.remove("active")
+        }
+        console.log(main)
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// countryInput.onkeyup = (e) => {
+//     let countryData = e.target.value;
+
+//     if (countryData) {
+//         main.classList.add("active");
+//     } else {
+//         main.classList.remove("active")
+//     }
+//     console.log(main)
+// }
+
 
 // let url = 'https://v6.exchangerate-api.com/v6/bea4b32831177324429bcd29/latest/USD';
 
@@ -11,7 +50,3 @@
 // for(i = 0; i < countryId.length; i++) {
   //  console.log(countryId[i].code)
 // }
-
-countryInput = document.querySelectorAll(".country-input");
-
-console.log(countryInput)
