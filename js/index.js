c1634c7fdfd56f7366c4e44d92f43fba14b317d7
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-08-26 18:28:06 +0100

    styling app

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 
-let url = 'https://v6.exchangerate-api.com/v6/bea4b32831177324429bcd29/latest/USD';
+// let url = 'https://v6.exchangerate-api.com/v6/bea4b32831177324429bcd29/latest/USD';
 
-fetch(url).then(res => res.json()).then(result => {
-    console.log(result);
- })
+// fetch(url).then(res => res.json()).then(result => {
+  //  console.log(result);
+ // })
 
-let countryId = countryCodes;
-console.log(countryId)
+// let countryId = countryCodes;
+// console.log(countryId)
 
-for(i = 0; i < countryId.length; i++) {
-    console.log(countryId[i].code)
-}
+// for(i = 0; i < countryId.length; i++) {
+  //  console.log(countryId[i].code)
+// }
+
+countryInput = document.querySelectorAll(".country-input");
+
+console.log(countryInput)
